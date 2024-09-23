c49e1c234bf4ec78f9fcf1d760c12517becd3756
Pranish2005 <168003204+Pranish2005@users.noreply.github.com>
2024-09-23 21:07:35 +0530

    visualization.c

--- visualization.c
+++ visualization.c
@@ -1,42 +1,54 @@
 #include <graphics.h>
 #include <conio.h>
-#include <math.h>
-#include <stdlib.h>
+#include <stdio.h>
 
 
-struct node
-{
-    struct node* next;
-    struct t_node* data;
-};
-
-struct node* new_qNode(struct t_node* x)
-{
-    struct node* temp=(struct node*)malloc(sizeof(struct node));
-    temp->next=NULL;
-    temp->data=x;
-    return temp;
-}
-
-struct t_node
-{
+struct t_node {
+    int data;
     struct t_node* left;
     struct t_node* right;
-    int data;
 };
 
-struct t_node* new_tNode(int x)
-{
-    struct t_node* temp=(struct t_node*)malloc(sizeof(struct t_node));
-    temp->left=temp->right=NULL;
-    temp->data=x;
-    return temp;
+struct t_node *root;
+
+
+struct t_node* new_tNode(struct t_node *root,int val) {
+    struct t_node *ptr, *nodeptr, *parentptr;
+    ptr = (struct t_node*)malloc(sizeof(struct t_node));
+    ptr->data = val;
+    ptr->left = NULL;
+    ptr->right = NULL;
+    if(root==NULL)
+    {
+	root=ptr;
+	root->left=NULL;
+	root->right=NULL;
+    }
+    else
+    {
+	parentptr=NULL;
+	nodeptr=root;
+	while(nodeptr!=NULL)
+	{
+	    parentptr=nodeptr;
+	    if(val<nodeptr->data)
+		nodeptr=nodeptr->left;
+	    else
+		nodeptr = nodeptr->right;
+	}
+if(val<parentptr->data)
+parentptr->left = ptr;
+else
+parentptr->right = ptr;
 }
+return root;
+}
+
 
 void Inorder(struct t_node* root)
 {
     if(!root)
-        return;
+	return;
     Inorder(root->left);
     printf("%d ",root->data);
     Inorder(root->right);
@@ -45,7 +57,7 @@ void Inorder(struct t_node* root)
 void Preorder(struct t_node* root)
 {
     if(!root)
-        return;
+	return;
     printf("%d ",root->data);
     Preorder(root->left);
     Preorder(root->right);
@@ -54,21 +66,17 @@ void Preorder(struct t_node* root)
 void Postorder(struct t_node* root)
 {
     if(!root)
-        return;
+	return;
     Postorder(root->left);
     Postorder(root->right);
     printf("%d ",root->data);
 }
 
-void printTree(int x, int y, struct t_node* root, int index)
-{
-    struct t_node* left;
-    struct t_node* right;
-    int lx, ry;
+void printTree(int x, int y, struct t_node* root, int index) {
     char str[10];
-    
+
     if (!root)
-        return;
+	return;
 
     itoa(root->data, str, 10);
 
@@ -80,167 +88,60 @@ void printTree(int x, int y, struct t_node* root, int index)
 
     outtextxy(x - 2, y - 3, str);
     delay(200);
-    setcolor(GREEN);
 
-    left = root->left;
-    right = root->right;
-    lx = 2 * index + 1;
-    ry = 2 * index + 1;
-
-    printTree(x - y / (index + 1), y + 50, left, lx);
-    printTree(x + y / (index + 1), y + 50, right, ry);
-
-    if (left != NULL)
-    {
-        delay(200);
-        line(x, y + 14, x - y / (index + 1), y + 50 - 14);
+    if (root->left) {
+	setcolor(GREEN);
+	line(x, y + 14, x - 30, y + 50 - 14);
+	printTree(x - 30, y + 50, root->left, index * 2);
     }
-    if (right != NULL)
-    {
-        delay(200);
-        line(x, y + 14, x + y / (index + 1), y + 50 - 14);
+
+    if (root->right) {
+	setcolor(GREEN);
+	line(x, y + 14, x + 30, y + 50 - 14);
+	printTree(x + 30, y + 50, root->right, index * 2 + 1);
     }
 }
 
-struct t_node* findMin(struct t_node* root)
-{
-    while (root && root->left != NULL)
-	root = root->left;
-    return root;
-}
-
-struct t_node* deleteNode(struct t_node* root, int key)
-{
-     struct t_node* temp = findMin(root->right);
-    if (!root)
-	return NULL;
-
-    if (key < root->data)
-	root->left = deleteNode(root->left, key);
-    else if (key > root->data)
-	root->right = deleteNode(root->right, key);
-    else
-    {
-
-	if (!root->left)
-	{
-	    struct t_node* temp = root->right;
-	    free(root);
-	    return temp;
-	}
-	else if (!root->right)
-	{
-	    struct t_node* temp = root->left;
-	    free(root);
-	    return temp;
-	}
-
-
-        root->data = temp->data;
-        root->right = deleteNode(root->right, temp->data);
-    }
-    return root;
-}
-
-int main()
-{
-    int gd = DETECT, gm, i, x, lc, rc, c = 1, choice;
-    struct t_node* root;
-    struct t_node* temp;
-    struct node* head;
-    struct node* tail;
-    struct node* t;
+int main() {
+    int gd = DETECT, gm,n,val;
     initgraph(&gd, &gm, "C:\\TURBOC3\\BGI");
-    cleardevice();
-
-    printf("NOTE: For tree input take -1 as NULL\n\n");
-
-    printf("Enter root node data: ");
-    scanf("%d", &x);
-    root = new_tNode(x);
-    head = new_qNode(root);
-    tail = head; 
-
-    while (head)
+    do{
+    printf("\n1)Insert element.");
+    printf("\n2)Inorder Traversal");
+    printf("\n3)Postorder traversal.");
+    printf("\n4)Preorder traversal.");
+    printf("\n5)Visualization.");
+    printf("\n6)Exit/");
+    printf("\nEnter choice: ");
+    scanf("%d",&n);
+    switch(n)
     {
-        temp = head->data;
+	case 1:
+	    printf("\n Enter the value of the new node : ");
+	    scanf("%d", &val);
+	    root = new_tNode(root, val);
+	    break;
+	case 2:
+	    printf("\n\nThe Inorder traversal is : ");
+	    Inorder(root);
+	    break;
+	case 3:
+	    printf("\n\nThe Postorder traversal is : ");
+	    Postorder(root);
+	    break;
+	case 4:
+	    printf("\n\nThe Preorder traversal is : ");
+	    Preorder(root);
+	    break;
+	case 5:
+	    cleardevice();
+	    printTree(300, 100, root,0);
+	    break;
 
-        printf("Enter left child of %d: ", temp->data);
-        scanf("%d", &lc);
-        if (lc != -1)
-        {
-            temp->left = new_tNode(lc);
-            tail->next = new_qNode(temp->left);
-            tail = tail->next;
-        }
-
-        printf("Enter right child of %d: ", temp->data);
-        scanf("%d", &rc);
-        if (rc != -1)
-        {
-            temp->right = new_tNode(rc);
-            tail->next = new_qNode(temp->right);
-            tail = tail->next;
-        }
-        t = head;
-        head = head->next;
-        free(t);
-        printf("\n");
     }
+    }while(n!=6);
 
-    while (c)
-    {
-        printf("1. In-order Traversal\n2. Pre-order Traversal\n3. Post-order traversal\n4. Visualize Tree\n5. Delete Node\n6. Exit\nEnter your choice: ");
-        scanf("%d", &choice);
-        switch (choice)
-        {
-            case 1:
-            {
-                printf("In-order Traversal: ");
-                Inorder(root);
-                break;
-            }
-            case 2:
-            {
-                printf("Pre-order Traversal: ");
-                Preorder(root);
-                break;
-            }
-            case 3:
-            {
-                printf("Post-order Traversal: ");
-                Postorder(root);
-                break;
-            }
-            case 4:
-            {
-                cleardevice();
-                printTree(300, 100, root, 0);
-                getch();
-                cleardevice();
-                break;
-            }
-            case 5:
-            {
-                int key;
-                printf("Enter the value of the node to delete: ");
-                scanf("%d", &key);
-                root = deleteNode(root, key);
-                printf("Node deleted.\n");
-                break;
-            }
-            case 6:
-            {
-                c = 0;
-                break;
-            }
-            default:
-            {
-                printf("Invalid choice! Try again!\n");
-            }
-        }
-        printf("\n\n");
-    }
+
     getch();
     return 0;
 }
